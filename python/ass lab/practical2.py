c3449d0c4fc3e0406e84f8c00e884db074316b6e
Agrim Jain <agrimjain015@gmail.com>
2026-02-09 07:28:10 +0530

    commit

---
+++ python/ass lab/practical2.py
@@ -0,0 +1,11 @@
+# types of data structures
+## list 
+
+bus_numbers = [1, 2, 3, 4] # List of integers
+voltage = [220.0, 225.5, 230.0] # List of floats
+devices = ["Fan", "Light", "Heater"] # list of strings
+data = [1, 230.5, "Feeder A"] # Mixed list (int + float + string)
+print("Bus Numbers:", bus_numbers)
+print("Voltage Levels:", voltage)
+print("Devices:", devices)
+print("Mixed Data:", data)
