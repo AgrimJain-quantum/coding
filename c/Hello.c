f908b039f7f0223be53ecd2e55677ba1afd8f8a9
Agrim Jain <agrimjain015@gmail.com>
2026-05-20 22:09:41 +0530

    Add final_price and _age to Hello.c
    Introduce two new integers (_age and final_price) in c/Hello.c and print final_price. Small formatting cleanup (extra newline). Recompiled binaries c/Hello.exe and c/a.exe were updated accordingly.

--- c/Hello.c
+++ c/Hello.c
@@ -4,14 +4,27 @@
 int main()
 {
     /*THIS IS A SIMPLE TEST C PROGRAM CODE FILE */
+    /*allways rember that variables must be simple to use and understand*/
     int number = 25;
     char star = '*';
     int age = 30;
     float pi = 3.14;
+    int _age = 23;
+    int final_price = 1000;
+
     printf("the number is %d\n" , number);
     printf("the star is %c\n" , star);
     printf("the age is %d\n" , age);
     printf("the value of pi is %f\n" , pi);
+    printf("the price is %d\n", final_price);
+    printf("the g is %f\n", pi * 10);
+    /*the values that don't change are callled as constants */
+    /* there are  3 types of constants 
+       1. integer constants
+       2. floating point constants
+       3. character constants
+    */
+   
     return 0;
 
 
