4d2de01e40eebd3b61a48688e5f0049dcb4f7ade
Agrim Jain <agrimjain015@gmail.com>
2026-05-21 12:45:01 +0530

    Add user input and sum; comment example prints
    Comment out the original hardcoded demo variables and printf calls, and add interactive input using scanf: prompt for a single number (printed back) and for two integers whose sum is computed and printed. Also tidy up comment formatting. Compiled binaries were updated accordingly.

--- c/Hello.c
+++ c/Hello.c
@@ -5,19 +5,19 @@ int main()
 {
     /*THIS IS A SIMPLE TEST C PROGRAM CODE FILE */
     /*allways rember that variables must be simple to use and understand*/
-    int number = 25;
-    char star = '*';
-    int age = 30;
-    float pi = 3.14;
-    int _age = 23;
-    int final_price = 1000;
+    // int number = 25;
+    // char star = '*';
+    // int age = 30;
+    // float pi = 3.14;
+    // int _age = 23;
+    // int final_price = 1000;
 
-    printf("the number is %d\n" , number);
-    printf("the star is %c\n" , star);
-    printf("the age is %d\n" , age);
-    printf("the value of pi is %f\n" , pi);
-    printf("the price is %d\n", final_price);
-    printf("the g is %f\n", pi * 10);
+    // printf("the number is %d\n" , number);
+    // printf("the star is %c\n" , star);
+    // printf("the age is %d\n" , age);
+    // printf("the value of pi is %f\n" , pi);
+    // printf("the price is %d\n", final_price);
+    // printf("the g is %f\n", pi * 10);
     
     /*the values that don't change are callled as constants */
     /* there are  3 types of constants 
@@ -25,13 +25,26 @@ int main()
        2. floating point constants
        3. character constants
     */;
-    // comments are also of two types 
-    // 1. single line commments 
-    // 2. multi line comments 
+    /*comments are also of two types 
+    1. single line commments 
+    2. multi line comments */
     /* %d, %f,%c these are format specifiers 
     and printf funtion is a library function*/
+    int number;
+    printf("enter a number :");
+    scanf("%d", &number);
+    printf("the number is %d\n" , number);
+    int a, b;
+    printf("enter a");
+    scanf("%d", &a);
+    printf("enter b");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("the sum of a and b is %d\n", sum);
 
-    
+
+
+
 
 
 
