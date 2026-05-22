c8635adbe5d6fa04d0650d9c12f3aff958a50445
Agrim Jain <agrimjain015@gmail.com>
2026-05-22 20:27:23 +0530

    Comment out demo code and adjust expression
    Commented out initial example variable declarations and prints in c/chapter2.c to leave instructional text visible without executing demo code. Clarified an arithmetic example by adding parentheses (5+(2/2)*3) to make operator precedence explicit. Compiled binary c/chapter2.exe was updated to reflect the source changes.

--- c/chapter2.c
+++ c/chapter2.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int a = 10;
-    int b = 20;
-    int sum = a + b;
-    printf("The sum of %d and %d is %d\n", a, b, sum);
-    int power  = pow(a, b);
-    printf("%d raised to the power of %d is %d\n", a, b, power);
-    int c = 2;
-    float d = 2.0;
-    printf("%f\n", c*d);
+//     int a = 10;
+//     int b = 20;
+//     int sum = a + b;
+//     printf("The sum of %d and %d is %d\n", a, b, sum);
+//     int power  = pow(a, b);
+//     printf("%d raised to the power of %d is %d\n", a, b, power);
+//     int c = 2;
+//     float d = 2.0;
+//     printf("%f\n", c*d);
     /* INSTRUCTIONS
     THESE ARE STATEMENTS IN A PROGRAM 
     IT CAN BE OF THREE TYPES 
@@ -35,22 +35,22 @@ int main(){
             EG - IF ELSE, SWITCH CASE, FOR LOOP, WHILE LOOP, DO WHILE LOOP
        "^" this a xor operator which is used to compare two values and returns true if only one of the values is true
     */;
-    int q = 1.999999;
-    printf("%d\n ", q);
-    /* operator precedence
-        1. Parentheses ()
-        2. Exponents
-        3. Multiplication and Division
-        4. Addition and Subtraction
-        5. Assignment
-    */
-   int x = 100;
-   int y = 200;
-   printf("%d\n", x * y / a * b); // associativity is left to right for multiplication and division
+//     int q = 1.999999;
+//     printf("%d\n ", q);
+// //     /* operator precedence
+//         1. Parentheses ()
+//         2. Exponents
+//         3. Multiplication and Division
+//         4. Addition and Subtraction
+//         5. Assignment
+//     */
+//    int x = 100;
+//    int y = 200;
+//    printf("%d\n", x * y / a * b); // associativity is left to right for multiplication and division
    printf("%d\n", 5*2-2*3);
    printf("%d\n", 5*3/2*3);
    printf("%d\n", 5*(2/2)*3);
-   printf("%d\n", 5+2/2*3);
+   printf("%d\n", 5+(2/2)*3);
 
 
 
