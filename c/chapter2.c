51ef74cb7c621ed7607d85b7818b341432e46bdf
Agrim Jain <agrimjain015@gmail.com>
2026-05-23 11:13:16 +0530

    Clarify loop control comment in chapter2.c
    Update comment to specify that loop control refers to for and while loops and adjust surrounding whitespace for readability.

--- c/chapter2.c
+++ c/chapter2.c
@@ -67,7 +67,8 @@ int main(){
    // these are of four types 
    // 1. sequence control
    // 2. decision making control } if else will start form here
-   // 3. loop control
+   // 3. loop control } for and while loops 
+   
    // 4. case control
 
 
