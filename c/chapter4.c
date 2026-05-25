1581a342292b6a6accc2548a67ecaa8684656d52
Agrim Jain <agrimjain015@gmail.com>
2026-05-25 14:03:38 +0530

    Add for-loop example to chapter4.c
    Introduce comments explaining for-loop syntax and add a for loop that prints "Hello world" five times in chapter4.c. The compiled binary a.exe was also updated to reflect the change.

--- c/chapter4.c
+++ c/chapter4.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main(){
     // loop cantrol statements
+    // for loop
+    // for (initialization; condition; increment/decrement){
+           // code to be executed
+    // }
     
-    
+    for (int i = 1; i <= 5; i += 1){
+        printf("Hello world ");
+    }
     return 0;
 
 }
