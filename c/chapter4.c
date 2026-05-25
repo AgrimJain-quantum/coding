6fa643dfc38de573b1a86fcc96dcb7abe9233e43
Agrim Jain <agrimjain015@gmail.com>
2026-05-25 18:06:09 +0530

    Insert blank line before return in chapter4.c
    Add an empty line before the `return 0;` statement in main() to improve readability. This is a whitespace-only change and does not affect program behavior. Note: file currently lacks a trailing newline.

--- c/chapter4.c
+++ c/chapter4.c
@@ -6,9 +6,35 @@ int main(){
            // code to be executed
     // }
     
-    for (int i = 1; i <= 5; i += 1){
-        printf("Hello world ");
-    }
+    // for (int i = 1; i <= 5; i += 1){
+    //     printf("Hello world\n ");
+    // }
+    // for (int i = 10; i <= 100; i += 1){
+    //     printf("%d\n", i);
+
+    // }
+    // increment operators 
+    // i++ or ++i
+    // i++ is post increment operator which means it will return the value of i before incrementing it
+    // ++i is pre increment operator which means it will return the value of i after incrementing it            
+
+
+    // iterator or counter variable can be declared outside the loop as well
+    // for (int i = 10 ; i>= 1; i -= 1){
+    //     printf("%d\n", i);
+    // }
+    // for (int i = 0; i<= 10; i += 1){
+    //     printf("%d\n", i);
+    // }
+
+    int i = 1;
+    printf("%d\n", i++);
+    printf("%d\n", i);
+
+    printf("%d\n", ++i);
+    printf("%d\n", i);
+    
+
     return 0;
 
 }
