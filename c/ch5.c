0a2b5e51b739c0063de1ef28f66432ca50394031
Agrim Jain <agrimjain015@gmail.com>
2026-05-28 00:20:04 +0530

    Add print_hello and call it in loop
    Declare and define a print_hello() helper that prints 'Hello World', and invoke it five times from main using a for loop. Also includes updated/added compiled binaries (a.exe updated, ch5.exe added).

--- c/ch5.c
+++ c/ch5.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
+void print_hello(); // declaration
 int main(){
+    // print_hello(); // fuction call
 
-    
+    // return type - void(will not return anything)
+    // function name - main
+    // parameter list - void (will not take any parameters) 
+    for (int i = 0; i<5; i++){
+        print_hello();
+    }
+   
+
     return 0;
 
+}
+void print_hello(){
+    printf("Hello World\n"); // function definition
 }
