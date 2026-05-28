262e8d0940bb7f854baaac56b5e3bf6e4d7b1d95
Agrim Jain <agrimjain015@gmail.com>
2026-05-28 18:14:43 +0530

    Comment out sum demo and add factorial note
    Commented out the recursive sum function and its main/test invocation in ch5.c, removing the active demo code. Added a placeholder comment indicating an upcoming factorial implementation or change.

--- c/ch5.c
+++ c/ch5.c
@@ -130,18 +130,21 @@
 //     printhw(count - 1);
 // }
 
-int sum(int n);
-int main(){
-    printf("sum is: %d", sum(5));
+// int sum(int n);
+// int main(){
+//     printf("sum is: %d", sum(5));
 
-    return 0;
-}
-int sum(int n){
-    if (n == 1){
-        return 1;
-    }
-    int sumNm1 = sum(n-1);
-    int sumN = sumNm1 + n;
-    return sumN;
-}
+//     return 0;
+// }
+// int sum(int n){
+//     if (n == 1){
+//         return 1;
+//     }
+//     int sumNm1 = sum(n-1);
+//     int sumN = sumNm1 + n;
+//     return sumN;
+// }
+
+// q factorial of a number n
+
 
