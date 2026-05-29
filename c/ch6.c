f56c994263563e56faa23658e0f5786cdef42285
Agrim Jain <agrimjain015@gmail.com>
2026-05-29 16:11:01 +0530

    Add pointer example stub in ch6.c
    Create ch6.c with a brief pointers comment, include <stdio.h> and a main() stub that declares `int a = 10` and an unfinished `int q` assignment. This is a work-in-progress placeholder for pointer examples that needs completion.

--- c/ch6.c
+++ c/ch6.c
@@ -1,1 +1,18 @@
+// pointers - a variable that stores the memory address of another variable
 #include<stdio.h>
+int main(){
+    int a = 10;
+    int *p = &a; // "*" is the dereference operator, it gives us the value stored at the memory address that p is pointing to
+    // "& " is the address-of operator, it gives us the memory address of the variable a
+    int _age = *p;
+    int *q = &_age;
+
+    printf("The value of a is: %d\n", a);
+    printf("The value of p is: %p\n", p);
+    printf("The value of _age is: %d\n", _age);
+    printf("The value of q is: %p\n", q);
+     
+
+    return 0;
+}
+
