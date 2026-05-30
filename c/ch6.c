4142a8343cbe7389f159cc9d189e77ace403203d
Agrim Jain <agrimjain015@gmail.com>
2026-05-30 12:57:31 +0530

    Add pointer-based _swap and demo call-by-reference
    Introduce _swap(int *a, int *b) with prototype and implementation to swap values by reference, and invoke it from main (with prints) to demonstrate call-by-reference behavior. Preserve the existing swap(int,int) (call-by-value) for comparison. Updated compiled binary a.exe as part of the changes.

--- c/ch6.c
+++ c/ch6.c
@@ -90,15 +90,28 @@
 
 // q swap 2 numbers using a, b
 void swap (int a, int b);
+void _swap (int *a, int *b);
 int main(){
     int x = 5, y = 10;
     swap(x, y);
+    printf("x = %d, y = %d\n", x, y);
+    _swap(&x, &y);
     printf("x = %d, y = %d\n", x, y);
 
 
 
     return 0;
 }
+// call by refrence
+void _swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b =t;
+    printf("a = %d, b = %d\n", *a, *b);
+}
+
+
+// call by value
 void swap (int a, int b){
     int t = a;
     a = b;
