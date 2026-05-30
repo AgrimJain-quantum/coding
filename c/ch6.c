50e5488d7d6c2024870cfb84d1a90c66393b7508
Agrim Jain <agrimjain015@gmail.com>
2026-05-30 09:55:34 +0530

    Comment out pointer examples in ch6.c
    Commented out the pointer demonstration code and related printf statements in c/ch6.c (single/double pointers, pointer arithmetic, and example prints). This silences example output and simplifies the file for further edits or testing.

--- c/ch6.c
+++ c/ch6.c
@@ -1,53 +1,55 @@
 // pointers - a variable that stores the memory address of another variable
 #include<stdio.h>
 int main(){
-    int a = 10;
-    int *p = &a; // "*" is the dereference operator, it gives us the value stored at the memory address that p is pointing to
+    // int a = 10;
+    // int *p = &a; // "*" is the dereference operator, it gives us the value stored at the memory address that p is pointing to
     // "& " is the address-of operator, it gives us the memory address of the variable a
-    int _age = *p;
-    int *q = &_age;
+    // int _age = *p;
+    // int *q = &_age;
 
-    printf("The value of a is: %d\n", a);
-    printf("The value of p is: %p\n", p);
-    printf("The value of _age is: %d\n", _age);
-    printf("The value of q is: %p\n", q);
+    // printf("The value of a is: %d\n", a);
+    // printf("The value of p is: %p\n", p);
+    // printf("The value of _age is: %d\n", _age);
+    // printf("The value of q is: %p\n", q);
 
-    int w = 20;
-    int *r = &w;
-    printf("the value of w is: %p\n", r);
-    printf("the value of w in the signed format is: %u\n", r);
-    printf("the value of the w is: %d\n", *r);
+    // int w = 20;
+    // int *r = &w;
+    // printf("the value of w is: %p\n", r);
+    // printf("the value of w in the signed format is: %u\n", r);
+    // printf("the value of the w is: %d\n", *r);
     // %u is for unsigned int , 
     // %p is for pointer address = will print a address in a hexa decimal format 
-    printf("%d\n", w);
-    printf("%d\n", *r);
-    printf("%d\n", *(&w));
+    // printf("%d\n", w);
+    // printf("%d\n", *r);
+    // printf("%d\n", *(&w));
 
     // practice q 36
-    int *ptr;
-    int x;
+    // int *ptr;
+    // int x;
 
-    ptr = &x;
-    *ptr = 0;
-    printf("x = %d\n", x);
-    printf("*ptr = %d\n", *ptr);
+    // ptr = &x;
+    // *ptr = 0;
+    // printf("x = %d\n", x);
+    // printf("*ptr = %d\n", *ptr);
     
-    *ptr += 5;
-    printf("x = %d\n", x);
-    printf("*ptr = %d\n", *ptr);
+    // *ptr += 5;
+    // printf("x = %d\n", x);
+    // printf("*ptr = %d\n", *ptr);
 
-    (*ptr)++;
-    printf("x = %d\n", x);
-    printf("*ptr = %d\n", *ptr);
+    // (*ptr)++;
+    // printf("x = %d\n", x);
+    // printf("*ptr = %d\n", *ptr);
 
-    float f = 3.14;
-    float *fp = &f;
-    float **fpp = &fp;
-    printf("The value of f is: %f\n", f);
-    printf("The value of fp is: %p\n", fp); 
-    printf("The value of fpp is: %p\n", fpp);
-    printf("The value of *fp is: %f\n", *fp);
-    printf("The value of **fpp is: %f\n", **fpp);
+    // float f = 3.14;
+    // float *fp = &f;
+    // float **fpp = &fp;
+    // printf("The value of f is: %f\n", f);
+    // printf("The value of fp is: %p\n", fp); 
+    // printf("The value of fpp is: %p\n", fpp);
+    // printf("The value of *fp is: %f\n", *fp);
+    // printf("The value of **fpp is: %f\n", **fpp);
+
+    
 
 
 
