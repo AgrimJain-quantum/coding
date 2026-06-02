eadacd40bf08c87d15cb0e80852a062b156cafab
Agrim Jain <agrimjain015@gmail.com>
2026-06-02 20:53:51 +0530

    Comment out sample prints in ch8.c
    Commented out the sample string declarations and printf calls in c/ch8.c, leaving main with only return 0. This temporarily disables printing of the example `name` and `class` values while preserving the code for future use.

--- c/ch8.c
+++ c/ch8.c
@@ -1,9 +1,30 @@
 #include<stdio.h>
 int main(){
 
-    char name[] = {'a','g','r','i','m','\0'};
-    char class[] = "C Programming";
-    printf("%s\n", name);
-    printf("%s\n", class);
+    // char name[] = {'a','g','r','i','m','\0'};
+    // char class[] = "C Programming";
+    // printf("%s\n", name);
+    // printf("%s\n", class);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
