9ee547bf131100db40996e217814757a3b24928e
Agrim Jain <agrimjain015@gmail.com>
2026-06-02 21:07:34 +0530

    Print characters of first and last name
    Add loops in c/ch8.c to iterate over the firstname and lastname arrays and print each character followed by a tab, with a newline after each name. The compiled binary (c/a.exe) was also updated to reflect the change.

--- c/ch8.c
+++ c/ch8.c
@@ -7,11 +7,15 @@ int main(){
     // printf("%s\n", class);
     char firstname[] = "Agrim";
     char lastname[] = "Jain";
+    for(int i = 0; firstname[i] != '\0'; i++){
+        printf("%c\t", firstname[i]);
+    }
+    printf("\n");
+    for(int i = 0; lastname[i] != '\0'; i++){
+        printf("%c\t", lastname[i]);
+    }
+    printf("\n");
     
-
-
-
-
 
 
 
