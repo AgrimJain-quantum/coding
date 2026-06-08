a7f37be7a84880e720d445101b0a9f4815787481
Agrim Jain <agrimjain015@gmail.com>
2026-06-08 19:34:24 +0530

    feat: add car structure and boilerplate implementation to ch9.c

--- c/ch9.c
+++ c/ch9.c
@@ -1,5 +1,44 @@
 #include<stdio.h>
+#include<string.h>
+
+struct car{
+    char name[100];
+    int year;
+    float hp;
+};
+
+
 int main(){
+    // struct car c1;
+    // c1.year = 2020;
+    // c1.hp = 150.5;
+    // strcpy(c1.name, "Toyota");
+
+    // struct car c2;
+    // c2.year = 2022;
+    // c2.hp = 200.5;
+    // strcpy(c2.name, "Honda");
+    // printf("The name of the car is : %s", c1.name);
+    // printf("The year of the car is : %d\n", c1.year);
+    // printf("The horsepower of the car is : %f hp\n", c1.hp);
+    // printf("The name of the car is : %s", c2.name);
+    // printf("The year of the car is : %d\n", c2.year);
+    // printf("The horsepower of the car is : %f hp\n", c2.hp);
+
+    // printf("Enter the name of the car : ");
+    // fgets(c1.name, 100, stdin);
+    // printf("Enter the model of the car : ");
+    // fgets(c1.model, 100, stdin);
+    // printf("Enter the year of the car : ");
+    // scanf("%d", &c1.year);
+    // printf("Enter the horsepower of the car : ");
+    // scanf("%f", &c1.hp);
+    // printf("The name of the car is : %s", c1.name);
+    // printf("The model of the car is : %s", c1.model);
+    // printf("The year of the car is : %d\n", c1.year);
+    // printf("The horsepower of the car is : %f hp\n", c1.hp);
+
+
 
     return 0;
 }
