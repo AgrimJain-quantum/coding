02734d994229ef41f8ef6579e2697ff00a623da6
Agrim Jain <agrimjain015@gmail.com>
2026-06-13 23:22:59 +0530

    commit

--- c/ch9.c
+++ c/ch9.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 
-struct car{
+// struct car{
+//     char name[100];
+//     char model[100];
+//     int year;
+//     float hp;
+// };
+
+ typedef struct carr{
     char name[100];
     char model[100];
     int year;
     float hp;
-};
+    }volk;
 
-void printcar(struct car c);
+// void printcar(struct car c);
 
 int main(){
     // struct car c1;
@@ -60,19 +67,49 @@ int main(){
     // printf("student -> hp = %.2f hp\n", ptr->hp);
 
     // passing structure toi function
-    struct car c1 = {"toyota", "camry", 2020, 160.5};
-    printcar(c1);
-    struct car c2 = {"honda", "civic", 2022, 180.5};
-    printcar(c2);
-    struct car c3 = {"bmw", "m3 competition", 2023, 503.0};
-    printcar(c3);
+    // struct car c1 = {"toyota", "camry", 2020, 160.5};
+    // printcar(c1);
+    // struct car c2 = {"honda", "civic", 2022, 180.5};
+    // printcar(c2);
+    // struct car c3 = {"bmw", "m3 competition", 2023, 503.0};
+    // printcar(c3);
+    //the data cant be changed in the function because we are passing the structure by value and not by reference
+
+    // typedef keywords 
+    
+    volk v1 = {"volkswagen", "golf", 2021, 170.5};
+    printf("The name of the car is : %s\n", v1.name);
+    printf("The model of the car is : %s\n", v1.model);
+    printf("The year of the car is : %d\n", v1.year);
+    printf("The horsepower of the car is : %.2f hp\n", v1.hp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
-void printcar(struct car c){
-    printf("car information : \n");
-    printf("The name of the car is : %s\n", c.name);
-    printf("the model of the car is : %s\n", c.model);
-    printf("The year of the car is : %d\n", c.year);
-    printf("The horsepower of the car is : %.2f hp\n", c.hp);
+// void printcar(struct car c){
+//     printf("car information : \n");
+//     printf("The name of the car is : %s\n", c.name);
+//     printf("the model of the car is : %s\n", c.model);
+//     printf("The year of the car is : %d\n", c.year);
+//     printf("The horsepower of the car is : %.2f hp\n", c.hp);
 
-}
+// }
