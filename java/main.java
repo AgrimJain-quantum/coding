dd1c518ace1baea5d4300ff5f6c3725217539d4c
Agrim Jain <agrimjain015@gmail.com>
2026-07-27 10:53:04 +0530

    java first program

--- java/main.java
+++ java/main.java
@@ -2,6 +2,15 @@ public class main {
     public static void main (String[] args) {
         System.out.println("Hello world");
         System.out.println("hi my name is agrim jain");
+        System.out.println("i am a studnt of muj\n");
+        // single line comment
+        /*
+        this 
+        is 
+        a 
+        multi line 
+        comment
+        */
         
     }
 }
