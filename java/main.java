c1f58d15883e28561befda5a29d1746761c26771
Agrim Jain <agrimjain015@gmail.com>
2026-07-27 10:02:48 +0530

    commit

--- java/main.java
+++ java/main.java
@@ -1,7 +1,5 @@
-package coding.java;
-
 public class main {
-    public static void main(){
-        
+    public static void main (String[] args) {
+        System.out.println("Hello world");
     }
 }
