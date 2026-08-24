ad8d3f42ddfe098b60587a79c911cac55dc4907b
Agrim Jain <agrimjain015@gmail.com>
2026-08-24 11:49:04 +0530

    commit

--- java/main.java
+++ java/main.java
@@ -1,17 +1,18 @@
-class Main {
+class main {
     public static void main (String[] args) {
         System.out.println("Hello world");
         System.out.println("hi my name is agrim jain");
-        System.out.println("i am a studnt of muj\n");
+        System.out.println("i am a student of muj\n");
         // single line comment
         /*
-        this 
-        is 
-        a 
-        multi line 
-        comment
+          this 
+          is 
+          a 
+          multi line 
+          comment
         */
        System.out.println("this is a multi line comment");
+       System.out.println("this is a single line comment");
        
     }
 }
